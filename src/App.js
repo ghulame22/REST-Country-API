9346cdb4083a788c31b47ea9ace80ce64e4f4099
Mustafa <ghulame22@gmail.com>
2023-01-12 15:38:02 +0530

    updating code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/CardPage" element={<CardPage />} />
+        {/* <Route exact path="/CardPage" element={<CardPage />} /> */}
         </Routes>
       </Router>
     </>
